4f9c0e56b6f4e3d5215b701da70e764af1889173
૮ ˶ᵔ ᵕ ᵔ˶ ა <101676768+redbloom@users.noreply.github.com>
2025-10-20 19:06:22 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prueba_SCISA_Michelle.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
