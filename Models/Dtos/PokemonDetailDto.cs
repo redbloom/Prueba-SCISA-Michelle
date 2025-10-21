aab53ebaec2a764eabd02a809fd182827f60605c
Michelle Ramírez <michelle.rmzruiz@outlook.com>
2025-10-21 15:50:06 -0600

    Modificacion en detalles de cada pokemon

--- Models/Dtos/PokemonDetailDto.cs
+++ Models/Dtos/PokemonDetailDto.cs
@@ -4,10 +4,14 @@ namespace Prueba_SCISA_Michelle.Models.Dtos
     {
         public int Id { get; set; }
         public string Name { get; set; } = "";
-        public int Height { get; set; }
-        public int Weight { get; set; }
+        public int Height { get; set; }          // dm
+        public int Weight { get; set; }          // hg
         public int BaseExperience { get; set; }
         public string ImageUrl { get; set; } = "";
         public string SpeciesName { get; set; } = "";
+
+        public decimal HeightMeters => Height / 10.0m;
+        public decimal WeightKg => Weight / 10.0m;
     }
+
 }
